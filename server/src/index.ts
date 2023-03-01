87005b48524bb8a32f8fbae7afa484460d7dde65
arnold <luizarnoldch@gmail.com>
2023-02-28 22:33:31 -0500

    server 4000

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hola mundo")
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(4000, () => {
+  console.log('Server listening on port 4000');
+});
